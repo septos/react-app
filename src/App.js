629ad830807a8660351a25aab6628601a1a5ab60
Septo Suharyanto <septosuharyanto@gmail.com>
2022-11-20 20:34:25 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import './App.css';
+import Introduction from "./components/introduction";
+
+function App() {
+  return (
+    <div>
+      <h1>Sesi 1</h1>
+      <Introduction />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/introduction.js
@@ -0,0 +1,11 @@
+function Introduction() {
+  return(
+    <div>
+      <h1>Introduction</h1>
+      <p>Introduction descripion, my name is septo</p>
+    </div>
+  );
+}
+
+
+export default Introduction;
